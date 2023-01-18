0f970515c1719a0c80b490dfaf1c2d094061433a
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-01-18 08:24:20 +0700

    Mengguanakan findViewID()

--- app/src/main/java/com/yp/myapplication/MainActivity.kt
+++ app/src/main/java/com/yp/myapplication/MainActivity.kt
@@ -2,11 +2,24 @@ package com.yp.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+
+//        Menampilkan layout Hello_world
         setContentView(R.layout.hello_world)
+
+//        Menangkap id komponen
+        val nameEditText: EditText = findViewById(R.id.nameEditText)
+        val sayHelloButton: Button = findViewById(R.id.sayHelloButton)
+        val sayHelloTextView: TextView = findViewById(R.id.sayHelloTextView)
+
+
+
+
     }
 }
