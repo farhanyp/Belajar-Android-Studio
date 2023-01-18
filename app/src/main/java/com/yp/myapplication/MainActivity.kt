d9afcda63fa9453f7d80df063fad72e37a22654c
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-01-18 08:52:05 +0700

    Menggunakan lateInit sebagai Best Practice untuk mendeklarasikan ID

--- app/src/main/java/com/yp/myapplication/MainActivity.kt
+++ app/src/main/java/com/yp/myapplication/MainActivity.kt
@@ -7,16 +7,33 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+//    Menggunakan lateinit bagian dari best practice untuk mendeklarasikan id
+    private lateinit var nameEditText: EditText
+    private lateinit var sayHelloButton: Button
+    private lateinit var sayHelloTextView: TextView
+
+//    Mendeklarasikan fungsi initComponents untuk menampkan id
+    private fun initComponents(){
+        nameEditText= findViewById(R.id.nameEditText)
+        sayHelloButton= findViewById(R.id.sayHelloButton)
+        sayHelloTextView= findViewById(R.id.sayHelloTextView)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 //        Menampilkan layout Hello_world
         setContentView(R.layout.hello_world)
 
+//        Memanggil Function initComponents
+        initComponents()
+
 //        Menangkap id komponen
-        val nameEditText: EditText = findViewById(R.id.nameEditText)
-        val sayHelloButton: Button = findViewById(R.id.sayHelloButton)
-        val sayHelloTextView: TextView = findViewById(R.id.sayHelloTextView)
+//        val nameEditText: EditText = findViewById(R.id.nameEditText)
+//        val sayHelloButton: Button = findViewById(R.id.sayHelloButton)
+//        val sayHelloTextView: TextView = findViewById(R.id.sayHelloTextView)
 
 //        Menggunakan click listener pada button
         sayHelloButton.setOnClickListener{
